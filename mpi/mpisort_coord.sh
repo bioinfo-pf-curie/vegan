dae98e5c367be0e115552159577183d4edb9e097
Frederic Jarlier <frederic.jarlier@curie.fr>
2020-10-26 18:45:01 +0100

    [ADD] first files

---
+++ mpi/mpisort_coord.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -o /mnt/beegfs/home/fjarlier/NEXTFLOW_MPI/output_coord_mpisort.%j.o
+#SBATCH -e /mnt/beegfs/home/fjarlier/NEXTFLOW_MPI/output_coord_mpisort.%j.e
+
+module load gnu8/8.3.0
+module load openmpi3
+
+MPISORT=$2
+SAM=$1
+OUTPUTDIR=$3
+
+mpirun $MPISORT $SAM $OUTPUTDIR -b -p -q 0
