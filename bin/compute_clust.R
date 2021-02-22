d50d78b0b825fd925a5231ad0bbc4479989a083c
tgutman <tom.gutman@curie.fr>
2021-02-22 16:45:25 +0100

    [MODIF] adapting R script for MultiQC + implementing Clustering in MultiQC

--- bin/compute_clust.R
+++ bin/compute_clust.R
@@ -12,10 +12,12 @@ figureName<-args[3]
 
 #inputTable <- "/bioinfo/users/tgutman/Documents/Tom/SeqOIA/SeqOIA_KDI/analysis/test_clustering/bam/clust_matrix.tsv"
 #outputDir<-"/data/users/tgutman/Documents/Tom/SeqOIA/SeqOIA_KDI/analysis/test_clustering/bam"
-#figureName<-"test_clust.png"
+#figureName<-"test_clust"
 
 # Handle path & Output Names
 outputFile=paste(outputDir,"/",figureName,sep = "")
+outputPlot=paste(outputDir,"/",figureName,".png",sep = "")
+outputMatrix=paste(outputDir,"/",figureName,"_identito.csv",sep = "")
 
 # Load libraries
 library(vegan)
@@ -38,30 +40,57 @@ clust_mat=read.table(inputTable, header=T, stringsAsFactors = F)
 rownames(clust_mat)=clust_mat$rs
 clust_mat=clust_mat[,-1]
 
+temp_colnames=colnames(clust_mat)
+temp_rownames=rownames(clust_mat)
+
 # Filter matrix
-small_clust_mat=clust_mat[, !sapply(clust_mat, is.character)]
+#small_clust_mat=clust_mat[, !sapply(clust_mat, is.character)]
+
+# Handle NA:
+clust_mat=as.matrix(clust_mat)
+clust_mat[which(clust_mat == "NEC")]=NA
+clust_mat=matrix(as.numeric(clust_mat),ncol=ncol(clust_mat))
+
+colnames(clust_mat)=temp_colnames
+rownames(clust_mat)=temp_rownames
+
+# Handle sample full of NA
+for (i in 1:nrow(clust_mat)){
+    if (sum(is.na(clust_mat[i,])) == ncol(clust_mat)){
+        warning('One sample contains only NAs, this sample will be removed')
+        clust_mat=clust_mat[-i,]
+        print(clust_mat)
+    }
+}
 
 # Compute distance
-#jacdist <- vegdist(clust_mat,method="jaccard",na.rm=TRUE)
-#jacdist_mat = as.matrix(jacdist)
-
-## reduce matrix
-jacdist <- vegdist(small_clust_mat,method="jaccard",na.rm=TRUE)
+jacdist <- vegdist(clust_mat,method="jaccard",na.rm=TRUE)
 jacdist_mat = as.matrix(jacdist)
 
+## reduce matrix
+#jacdist <- vegdist(small_clust_mat,method="jaccard",na.rm=TRUE)
+#jacdist_mat = as.matrix(jacdist)
+
+## 1-dist matrix:
+corr_mat= 1-jacdist_mat
+
 # Create heatmap
-clust=pheatmap(1-jacdist_mat, 
+clust=pheatmap(corr_mat,
          method = "Ward.D",
          show_rownames = TRUE,
          show_colnames = TRUE,
          clustering_distance_cols = "correlation",
          clustering_distance_rows = "correlation",
          display_numbers = TRUE,
-         legend = T,
-         fontsize = 20)
+         legend = TRUE ,
+         fontsize = 20,
+         silent = TRUE)
 
-# Save figure:
-save_pheatmap_png(clust, outputFile)
+ clust_order=clust$tree_col$order
+
+ # Save figure:
+ save_pheatmap_png(clust, outputPlot)
+ write.csv(corr_mat[clust_order,clust_order], outputMatrix, quote=TRUE, row.names=TRUE)
 
 # End message + Path:
 print("script ended successfully")
